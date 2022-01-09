52da0d60627e2291ee208a0220c5d12a15066325
Thurston Chen <thurston.chen@gmail.com>
2022-01-10 07:57:25 +0800

    test: add slim-local-model-training test to CI pipeline

--- scaffoldings/azure-mlops-mlflow-starterkit/src/run_training.py
+++ scaffoldings/azure-mlops-mlflow-starterkit/src/run_training.py
@@ -1,45 +1,53 @@
 import json
 import mlflow
 from azureml.core import Experiment
+from os import environ
 from pathlib import Path
-from utils.mlops_utils import get_aml_workspace
-
-# Load the JSON settings file and relevant section
-with open(Path("resources", "settings.json").as_posix()) as f:
-    settings = json.load(f)
-azureml_settings = settings["azureml"]
-experiment_settings = settings["experiment"]
-deployment_settings = settings["deployment"]
-compute_target_settings = settings["compute_target"]
+from src.utils.mlops_utils import get_aml_workspace
 
 
-# Get workspace
-ws = get_aml_workspace(azureml_settings["workspace_name"],
-                       azureml_settings["subscription_id"],
-                       azureml_settings["resource_group"])
+def train_model():
+    # Load configures from settings.json file
+    with open(Path("src/resources/settings.json").as_posix()) as f:
+        settings = json.load(f)
+
+    training_platform = environ.get("TRAINING_PLATFORM")
+    training_backend_config = None
+    use_conda = True
+    if training_platform == "azureml":
+        azureml_settings = settings["training"]["azureml"]
+        training_backend_config = {"COMPUTE": azureml_settings["compute_target"]["name"]}
+
+        # Get AML workspace
+        ws = get_aml_workspace(azureml_settings["workspace_name"],
+                               azureml_settings["subscription_id"],
+                               azureml_settings["resource_group"])
+
+        # Set mlflow tracking uri
+        mlflow.set_tracking_uri(ws.get_mlflow_tracking_uri())
+
+        # Attach AML experiment
+        exp = Experiment(workspace=ws,
+                         name=azureml_settings["experiment"]["name"])
+        mlflow.set_experiment(exp.name)
+    elif training_platform == "local":
+        azureml_settings = settings["training"]["local"]
+        use_conda = False
+
+    # Submit mlflow project
+    remote_mlflow_run = mlflow.projects.run(uri="src",
+                                            parameters={"epochs": azureml_settings["epochs"]},
+                                            backend=training_platform,
+                                            backend_config=training_backend_config,
+                                            use_conda=use_conda,
+                                            synchronous=True)
+
+    # Set output variable to Azure DevOps pipeline with experiment_name and
+    # run_id, then subsequent DevOps pipeline stages could find the target experiment run
+    if training_platform == "azureml":
+        print(f"##vso[task.setvariable variable=experimentName;isOutput=true]{exp.name}")
+        print(f"##vso[task.setvariable variable=experimentRunId;isOutput=true]{remote_mlflow_run.run_id}")
 
 
-# Set mlflow tracking
-mlflow.set_tracking_uri(ws.get_mlflow_tracking_uri())
-
-# os.environ["MLFLOW_TRACKING_URI"] = ws.get_mlflow_tracking_uri()
-
-# Attach Experiment
-exp = Experiment(workspace=ws,
-                 name=experiment_settings["name"])
-mlflow.set_experiment(exp.name)
-
-
-# Submit Project
-remote_mlflow_run = mlflow.projects.run(uri=".",
-                                        parameters={"epochs": 200},
-                                        backend="azureml",
-                                        backend_config={"COMPUTE": compute_target_settings["training"]["amlcompute"]["name"],
-                                                        "USE_CONDA": True},
-                                        synchronous=True)
-
-
-# Set output variable to Azure DevOps pipeline
-# With experiment_name and run_id, subsequent DevOps pipeline stages could find the target experiment run
-print(f"##vso[task.setvariable variable=experimentName;isOutput=true]{exp.name}")
-print(f"##vso[task.setvariable variable=experimentRunId;isOutput=true]{remote_mlflow_run.run_id}")
+if __name__ == '__main__':
+    train_model()
